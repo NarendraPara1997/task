9caa96f0dad525ad44f42ecaa60724d013d7705f
Para Narendra <para.narendra@eccouncil.org>
2023-07-27 10:26:18 +0530

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'notification';
+    title = 'notification';
+    webhook: boolean = false;
+    smsValue: boolean = false;
+    webhookOne: boolean = false;
+    smsValueOne: boolean = false;
+    webhookChnage() {
+        this.webhook = true;
+    }
+    SMSChnage(){
+        this.smsValue = true;
+    }
+    webhookChnageOne() {
+        this.webhookOne = true;
+    }
+    SMSChnageOne(){
+        this.smsValueOne = true;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
